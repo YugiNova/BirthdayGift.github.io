7a7a190c45e7d9b81e19b20567521982bdc21c5b
Yugi Nova <yuginovaniac@gmail.com>
2023-03-10 16:56:45 +0700

    eyes follow

--- js/main.js
+++ js/main.js
@@ -40,3 +40,16 @@ function count_down() {
     d_sec.innerHTML = s;
     
     if (gap <= 0) { var text="Happy New Year" + " " + new Date().getFullYear(); var content=document.querySelector('.content'); var countDown=document.querySelector('.countDown'); countDown.classList.add('newyear'); content.classList.add('newyear'); content.innerHTML=text; } } setInterval(function () { count_down(); }, 1000);
+
+    //eyes follow handle
+    document.querySelector("body").addEventListener("mousemove", eyeball);
+    function eyeball() {
+      const eye = document.querySelectorAll(".eye");
+      eye.forEach(function (eye) {
+        let x = eye.getBoundingClientRect().left + eye.clientWidth / 2;
+        let y = eye.getBoundingClientRect().top + eye.clientHeight / 2;
+        let radian = Math.atan2((event.pageX - x), (event.pageY - y));
+        let rotate = radian * (180 / Math.PI) * -1 + 270;
+        eye.style.transform = "rotate(" + rotate + "deg)";
+      });
+    }
