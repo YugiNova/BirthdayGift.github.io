1cae71b304770567833518bad20bd5765285a69c
Yugi Nova <yuginovaniac@gmail.com>
2023-03-23 21:38:03 +0700

    release test 1

--- js/main.js
+++ js/main.js
@@ -6,94 +6,17 @@
 //It also supports NodeList
 // VanillaTilt.init(document.querySelectorAll(".calendar-card"));
 
-let lastWishesHtml = `<div class="card-slider">
-    <div class="swiper-wrapper">
-
-      <!-- Happy -->
-      <div class="swiper-slide">
-        <div class="gift-card fire">
-          <div class="cover">
-            <h2 onclick="wishesAnimation(this)"><span style="font-size: 15px;">>> Tap me <<</span><br>First <br> wishes</h2>
-            <h1>Happy</h1>
-          </div>
-          <div class="wishes">
-            <p>There is no value in life except what you choose to place upon it and no happiness in any place except what you bring to it yourself. Have a wonderful time and a very happy birthday!</p>
-          </div>
-        </div>
-      </div>
-
-      <!-- Luck -->
-      <div class="swiper-slide">
-        <div class="gift-card water">
-          <div class="cover">
-            <h2 onclick="wishesAnimation(this)"><span style="font-size: 15px;">>> Tap me <<</span><br>Second <br> wishes</h2>
-            <h1>Luck</h1>
-          </div>
-          <div class="wishes">
-            <p>An old saying said that: “Every day is a gift. It can be really amazing, but it can be disappointing also. However, I want to wish good luck in everything that happens in your life. May yours bring you only pleasant and helpful gifts.</p>
-          </div>
-        </div>
-      </div>
-
-      <!-- Success -->
-      <div class="swiper-slide">
-        <div class="gift-card wood">
-          <div class="cover">
-            <h2 onclick="wishesAnimation(this)"><span style="font-size: 15px;">>> Tap me <<</span><br>Third <br> wishes</h2>
-            <h1>Success</h1>
-          </div>
-          <div class="wishes">
-            <p>All successful people men and women are big dreamers. They imagine what their future could be, ideal in every respect, and then they work every day toward their distant vision, that goal or purpose. For great success you must have big dreams.</p>
-          </div>
-        </div>
-      </div>
-
-      <!-- Health -->
-      <div class="swiper-slide">
-        <div class="gift-card earth">
-          <div class="cover">
-            <h2 onclick="wishesAnimation(this)"><span style="font-size: 15px;">>> Tap me <<</span><br>Fourth <br> wishes</h2>
-            <h1>Health</h1>
-          </div>
-          <div class="wishes">
-            <p>There’s nothing more important than our good health – that’s our principal capital asset. I wishes you have lots of health and success in your life.</p>
-          </div>
-        </div>
-      </div>
-
-      <!-- Write for yourselft next year -->
-      <div class="swiper-slide">
-        <div class="gift-card metal">
-          <div class="cover">
-            <h2 onclick="lastWishes(this)" style="font-size: 14px;">
-              I wishes you will keep both <span style="color:#cc8e35">healthy</span> and <span  style="color:#00b894">successful</span>, always get lots of <span  style="color:#3c67e3">luck</span>. And you will become the <span  style="color:#d63031">happiest</span> person in the world 
-              And there is another wish, the last wishes...<br>
-              >> Tap here <<
-            </h2>
-            <h2 class="second-cover" onclick="secondCover(this)">
-              The last wishes is your wishes to yourself on your next year's birthday, to you in the future,... <br>
-              It can be goals, promises, wishes for yourself next year and you can only revisit this day next year <br>
-              >> now tap if you ready to make this last wish <<
-            </h2>
-          </div>
-          <div class="last-wishes">
-            <textarea id="your-last-wishes" cols="35" placeholder="Type your wishes here, only English...."></textarea>
-            <!-- <input type="text" id="your-last-wishes"> -->
-            <button class="btn-send-wishes">
-              <span class="default">Send</span>
-              <span class="success">Sent</span>
-              <div class="left"></div>
-              <div class="right"></div>
-            </button>
-          </div>
-          <h2 class="last-cover">
-            Your wishes have been sent to the future <br>
-            You can only watch it again on your next year's birthday
-          </h2>
-        </div>
-      </div>
-    </div>
-    </div>`;
+let birthdayTextHtml = `<div class="happy-birthday">
+<ul class="c-rainbow">
+  <li class="c-rainbow__layer c-rainbow__layer--white">HappyBirthday</li>
+  <li class="c-rainbow__layer c-rainbow__layer--orange">HappyBirthday</li>
+  <li class="c-rainbow__layer c-rainbow__layer--red">HappyBirthday</li>
+  <li class="c-rainbow__layer c-rainbow__layer--violet">HappyBirthday</li>
+  <li class="c-rainbow__layer c-rainbow__layer--blue">HappyBirthday</li>
+  <li class="c-rainbow__layer c-rainbow__layer--green">HappyBirthday</li>
+  <li class="c-rainbow__layer c-rainbow__layer--yellow">HappyBirthday</li>
+</ul>
+</div>`;
 
 const myAtropos = Atropos({
   el: ".my-atropos",
@@ -102,7 +25,7 @@ const myAtropos = Atropos({
 });
 
 //count_down function
-var to_count = "23 March," + Math.floor(new Date().getFullYear());
+var to_count = "28 March," + Math.floor(new Date().getFullYear());
 function count_down() {
   
   var countDown = new Date(to_count).getTime();
@@ -124,21 +47,27 @@ function count_down() {
   var d_min = document.querySelector("#min");
   var d_sec = document.querySelector("#sec");
 
-  d_day.innerHTML = d;
-  d_hour.innerHTML = h;
-  d_min.innerHTML = m;
-  d_sec.innerHTML = s;
+  
 
   if (d === -1) {
     let countdown = document.querySelector(".countdown-timer");
-    countdown.style.display = "none";
+    countdown.style.height = "0px";
+    countdown.style.padding = "0px";
     let discover = document.querySelector(".discover-birthday");
-    discover.style.height = "140px";
+    discover.style.height = "auto";
     discover.style.padding = "10px";
   }
   else if(d < -1){
     to_count = "23 March," + Math.floor(new Date().getFullYear() + 1);
     console.log(to_count);
+    let discover = document.querySelector(".discover-birthday");
+    // discover.style.display = "none";
+  }
+  else{
+    d_day.innerHTML = d;
+    d_hour.innerHTML = h;
+    d_min.innerHTML = m;
+    d_sec.innerHTML = s;
   }
 }
 setInterval(function () {
@@ -161,9 +90,11 @@ function eyeball() {
 //Handle Countdown - Discover
 const countdownToogle = () => {
   let countdown = document.querySelector(".countdown-timer");
-  countdown.style.animation = "2s hide-countdown forwards";
+  countdown.style.animation = "2s hide-countdown forwards 1s";
   let discover = document.querySelector(".discover-birthday");
-  discover.style.animation = "2s show-discover forwards";
+  // discover.style.display = "block";
+  
+  discover.style.animation = "2s show-discover forwards 1s";
 };
 const showGift = () => {
   countdownToogle()
@@ -174,6 +105,8 @@ const showGift = () => {
 
 //click to discover gift 2023
 const clickToDiscover = () => {
+  //document.body.innerHTML = document.body.innerHTML.replace(`<div class="happy-text"></div>`,birthdayTextHtml);
+
   let circleTop = document.querySelector(".circle-top");
   circleTop.style.animation = "circle-top-move 4s forwards";
 
@@ -189,11 +122,11 @@ const clickToDiscover = () => {
   let calendarCard = document.querySelector(".container");
   calendarCard.style.animation = "disapear-calendar 3s forwards 1s";
 
-  
-  //document.body.innerHTML.replace(`<div class="card-slider"></div>`,lastWishesHtml);
-
   let swipperCard = document.querySelector(".card-slider");
   swipperCard.style.animation = "show-swipper 1s forwards 4s";
+
+  let happyText = document.querySelector(".happy-birthday");
+  happyText.style.animation = "show-happy-text 8s forwards";
 
   setTimeout(() => {
     circleTop.remove();
